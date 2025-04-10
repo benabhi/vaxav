f2f343adc157157e00bd8b0e923587d1d3498bf6
Hernan Jalabert <benabhi@gmail.com>
2025-04-10 16:20:26 +0000

    guardado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+    },
     server: {
         host: '0.0.0.0',
         port: 4321 // opcional, puedes especificar también el puerto
